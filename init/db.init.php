347f1a105faf50a9d5d77b71c80db97980b586e3
earngsreyphoung <earngsreyphoung@gmail.com>
2026-01-26 10:11:55 +0700

    week4

--- init/db.init.php
+++ init/db.init.php
@@ -5,7 +5,7 @@
     $db_pass = '';
     $db_port = 3306;
 
-    $db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    $db = new mysqli($db_host, $db_user, $db_pass, $db_name ,$db_port);
     if($db->connect_error){
         echo $db->connect_error;
         die();
--- pages/register.php
+++ pages/register.php
@@ -1,9 +1,12 @@
 <?php
-    if(isset($_POST['name'], $_POST['username'], $_POST['passwd'], $_POST['confirm_passwd'])){
-      $name = $_POST['name'];
-      $username= $_POST['username'];
-      $passwd = $_POST['passwd'];
-      $confirmpasswd =$_POST['confirmpasswd'];
+    $nameErr = $usernameErr = $passwdErr = '';
+    $name = $username = '';
+
+    if (isset($_POST['name'], $_POST['username'], $_POST['passwd'], $_POST['confirm_passwd'])){
+      $name = trim($_POST['name']);
+      $username= trim($_POST['username']);
+      $passwd = trim($_POST['passwd']);
+      $confirmpasswd = $_POST['confirm_passwd'];
       if(empty($name)){
         $nameErr = 'please input name!';
       }
@@ -11,31 +14,48 @@
         $usernameErr = 'please input username';
       }
       if(empty($passwd)){
-        $usernameErr = 'please input passwd';
+        $passwdErr = 'please input passwd';
+        
       }
       if($passwd !== $confirmpasswd){
-        $usernameErr = 'password do not match';
+        $passwd = 'password does not match!';
+      }
+      if(usernameExists($username)){
+        $usernameErr ='please choose another username!';
+      }
+      if(empty($nameErr)&& empty($usernameErr) && empty($passwdErr)){
+        if(registerUser($name,$username,$passwd)){
+          echo '<div class="alert alert-success" role="alert">Registration successful! You can now<a href="./?page=login" class="alert-link">login</a></div>';
+        }else{
+          echo '<div class ="alert alert-danger"role = "alert">Registration failed! Please try again.</div>';
+        }
       }
     }
 ?>
 
 <form method="post" action="./?page=register" class="col-md-8 col-lg-6 mx-auto">
-        <h1>Register Page</h1>
-  <div class="mb-3">
-    <label class="form-label">Name</label>
-    <input name="name" type="text" class="form-control">
+        <h3>Register Page</h3>
+        <div class="mb-3">
+        <label class="form-label">Name</label>
+        <input name="name" value="<?php echo $name?>" type="text" class="form-control 
+         <?php echo empty($nameErr) ? '' : 'is-invalid '; ?>">
+        <div class="invalid-feedback"> <?php echo $nameErr ?> </div>
   </div>
   <div class="mb-3">
     <label class="form-label">Username</label>
-    <input name="username" type="text" class="form-control">
+    <input name="username" value="<?php echo $username ?>"  type="text" class="form-control
+    <?php echo empty($usernameErr) ? '': 'is-invalid' ?>">
+    <div class="invalid-feedback"><?php echo$usernameErr?></div>
   </div>
   <div class="mb-3">
     <label class="form-label">Password</label>
-    <input name="password" type="password" class="form-control">
+    <input name="passwd" type="password" class="form-control
+    <?php echo empty($passwdErr) ? '': 'is-invalid' ?>">
+    <div class="invalid-feedback <?php echo$passwdErr?></div>">
   </div>
   <div class="mb-3">
     <label class="form-label">Confirm Password</label>
-    <input name="confirmpassword" type="password" class="form-control">
+    <input name="confirm_passwd" type="password" class="form-control">
   </div>
  
   <button type="submit" class="btn btn-primary">Submit</button>
